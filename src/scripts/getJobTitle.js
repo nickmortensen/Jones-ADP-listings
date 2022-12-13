81c922b9cbe817e90fedc9b82d7a5ce10bf43d1a
Nick Mortensen <nmortensen@jonessign.com>
2022-12-13 12:36:14 -0600

    additional job descriptsions & job Title tightening

--- src/scripts/getJobTitle.js
+++ src/scripts/getJobTitle.js
@@ -10,11 +10,20 @@ export default function getJobTitle( jobTitle ) {
 			title = 'Letter Fabricator';
 			break;
 		case 'Fabricator - Richmond':
-				title = 'Fabricator';
-				break;
+			title = 'Fabricator';
+			break;
 		case 'Fabricator - Green Bay':
-				title = 'Fabricator';
-				break;
+			title = 'Fabricator';
+			break;
+		case 'Painter/Paint Prepper - Green Bay':
+			title = 'Painter/Paint Prepper';
+			break;
+		case 'National Account Sales Executive - Signs & Branding':
+			title = 'National Sales Executive';
+			break;
+		case 'Project Manager - National Account (Signs & Branding)':
+			title = 'Project Manager - National Accounts';
+			break;
 		default:
 			title = jobTitle;
 
