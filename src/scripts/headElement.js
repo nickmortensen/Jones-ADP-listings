962ef43e0874b01637da93c864369fdb301c3cf5
nickmortensen <nmortensen@jonessign.com>
2023-04-19 12:19:58 -0500

    post seo meeting

---
+++ src/scripts/headElement.js
@@ -0,0 +1,54 @@
+
+export default function getHeadElement() {
+return `
+
+<style>.wf-force-outline-none[tabindex="-1"]:focus{outline:none;}</style>
+<meta charset="utf-8">
+<title>DEVELOP | National Sign Company | Jones Sign</title>
+
+<meta content="Be part of something visible. Join Jones Sign, a Nationwide leader in the Signage Industry. Click here now to view current openings!" name="description">
+<meta content="Careers | National Sign Company | Jones Sign" property="og:title">
+<meta content="Be part of something visible. Join Jones Sign, a Nationwide leader in the Signage Industry. Click here now to view current openings!" property="og:description">
+<meta content="https://assets.website-files.com/602b1199ad0366aa34b0d515/60bf766ac6f67f7e53278f5c_Jones%20Sign%20-%20National%20Sign%20Company.svg" property="og:image">
+
+<meta content="SEO Page" property="twitter:title">
+<meta content="Be part of something visible. Join Jones Sign, a Nationwide leader in the Signage Industry. Click here now to view current openings!" property="twitter:description">
+<meta content="https://assets.website-files.com/602b1199ad0366aa34b0d515/60bf766ac6f67f7e53278f5c_Jones%20Sign%20-%20National%20Sign%20Company.svg" property="twitter:image">
+<meta property="og:type" content="website">
+<meta content="summary_large_image" name="twitter:card">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<meta content="fxJxTR3x3Dqzva2ylprAUpaQo-1qaxLS_tbAQ42knHM" name="google-site-verification">
+<link href="https://assets.website-files.com/602b1199ad0366aa34b0d515/css/jonessign.f66ddf985.min.css" rel="stylesheet" type="text/css">
+
+<!-- ADD THIS HEIPRO DEV
+<link href="https://jobs.jonessign.com/styles/main.min.css" rel="stylesheet" type="text/css">
+end of CSS TO ADD-->
+
+<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700%7CRoboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic%7CRoboto+Condensed:300,300italic,regular,italic,700,700italic" media="all">
+<script type="text/javascript">WebFont.load({  google: {    families: ["Oswald:200,300,400,500,600,700","Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic","Roboto Condensed:300,300italic,regular,italic,700,700italic"]  }});</script><!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif]-->
+<script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+<link href="https://assets.website-files.com/602b1199ad0366aa34b0d515/60ad2345dd47b676496f108c_Jones-Favicon.png" rel="shortcut icon" type="image/x-icon">
+<link href="https://assets.website-files.com/602b1199ad0366aa34b0d515/60ad2357c18eb3c7278fa985_Jones-Webclip.png" rel="apple-touch-icon">
+
+<link href="https://www.jonessign.com//careers-national-sign-company" rel="canonical">
+
+
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-64841301-1"></script>
+<script type="text/javascript">window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-64841301-1', {'anonymize_ip': false});</script>
+<script id="jetboost-script" type="text/javascript"> window.JETBOOST_SITE_ID = "ckl8dy6rg0blx04uv0c3x2o4e"; (function() { d = document; s = d.createElement("script"); s.src = "https://cdn.jetboost.io/jetboost.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })(); </script>
+<script src="https://cdn.jetboost.io/jetboost.js" async=""></script><!-- [Attributes by Finsweet] Mirror click events -->
+
+<script defer="" src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorclick@1/mirrorclick.js"></script>
+<link href="https://assets.website-files.com/602b1199ad0366aa34b0d515/css/jonessign.f66ddf985.min.css" rel="stylesheet" type="text/css">
+
+<!-- calls to prism css for demonstration -->
+<link href="styles/prism.css" rel="stylesheet" />
+<script src="scripts/prism.js"></script>
+<link rel="stylesheet" href="styles/main.min.css">
+
+<script type="text/javascript" src="https://marketing.jonessign.com/analytics?conly=true&amp;visitor_id=125240168&amp;visitor_id_sign=869d9addddfb0485f6ad49a1dc8a1b2cc5a716fbd9b56ea5fa236e4d2342c75829b8be4290d754e634bad24d8234d63f519f253e&amp;pi_opt_in=&amp;campaign_id=55187&amp;account_id=928923&amp;title=Careers | National Sign Company | Jones Sign&amp;url=https://www.jonessign.com/careers-national-sign-company&amp;referrer="></script>
+
+`}
